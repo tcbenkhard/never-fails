e6b4151164ca0a38085afe713e7987cb4ae0b1b5
Timo Benkhard <timo.benkhard@nl.abnamro.com>
2021-02-22 13:20:19 +0100

    Use TESTNG

--- src/test/java/SuccessTest.java
+++ src/test/java/SuccessTest.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.testng.Assert.assertTrue;
 
 public class SuccessTest {
 
